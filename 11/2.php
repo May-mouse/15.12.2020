67721044026d6b20abc6acd086fa3629ca38ce70
may-may <vsirjdcrfz@mail.ru>
2021-01-04 21:53:07 +0400

    урок 11,исправление файлов 1 и 2 php.

--- 11/2.php
+++ 11/2.php
@@ -4,18 +4,37 @@
 </head>
 <body>
 <h1>Функции. Начало.</h1>
-<p>Создайте функцию getResult,
+<p>Задача 2. Создайте функцию getResult,
     которая будет принимать 3 значения:
     2 переменные над которыми будет
     производиться математическое действие
     и название самого действия, что должно быть произведено.</p>
+<?php
+function getResult($a1, $a2, $operation){
+    if ($operation == 'sum') {
+        $oper = ($a1 + $a2);
+        echo "сумма чисел  $a1 и $a2   равна  $oper" ;
+    }
+    if ($operation == 'sub') {
+        $oper = ($a1 - $a2);
+        echo "разница чисел  $a1 и $a2   равна  $oper" ;
+    }
+    if ($operation == 'div') {
+        $oper = ($a1 / $a2);
+        echo "частное чисел  $a1 и $a2   равно  $oper" ;
+    }
+    if ($operation == 'mult') {
+        $oper = ($a1 * $a2);
+        echo "произведение чисел  $a1 и $a2   равно  $oper" ;
+    }
+
+    if ($operation == 'expo') {
+        $oper = ($a1 ** $a2);
+        echo "число  $a1 в степени $a2  равно  $oper" ;
+    }
+}
+$result = getResult (150,9, 'mult');
+
+?>
 </body>
 </html>
-<?php
-
-function getreult($a1=50, $a2=10,$sum='сумма'){
-    $sum=$a1+$a2;
-    echo $sum;
-}
- getreult (25,6);
-?>
